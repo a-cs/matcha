fe429b02ab9566dbf1a5c93468c22c0c1c01b603
Rodrigo Felipe Dos Santos Longhi <rodrigo.longhi@mercadolivre.com>
2024-09-03 22:20:23 -0300

    create database connection

--- backend/main.go
+++ backend/main.go
@@ -1,14 +1,33 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+
+	_ "github.com/lib/pq"
 )
 
 func main() {
-	s := "gopher"
-	fmt.Printf("Hello and welcome, %s!\n", s)
+	dataSourceName := "user=myuser password=mysecretpassword host=localhost port=5432 dbname=mydatabase sslmode=disable"
+	db, err := sql.Open("postgres", dataSourceName)
+	if err != nil {
+		panic(err.Error())
+	}
+	defer db.Close()
 
-	for i := 1; i <= 5; i++ {
-		fmt.Println("i =", 100/i)
+	// Executar uma consulta e imprimir os resultados
+	rows, err := db.Query("SELECT * FROM users")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var id int
+		var name string
+		if err := rows.Scan(&id, &name); err != nil {
+			panic(err.Error())
+		}
+		fmt.Println(id, name)
 	}
 }
