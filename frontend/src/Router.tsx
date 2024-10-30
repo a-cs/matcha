37b726f99269258139655d4d1eb56c135a9df283
a-cs <acarneirosousa@gmail.com>
2024-10-29 21:24:53 -0300

    creating login page

--- frontend/src/Router.tsx
+++ frontend/src/Router.tsx
@@ -3,12 +3,14 @@ import { Route, Routes } from 'react-router-dom'
 import Home from './pages/home'
 import SignUp from './pages/signup'
 import EmailConfirmation from './pages/emailConfirmation'
+import Login from './pages/login'
 
 export function Router() {
 	return (
 		<Routes>
 			<Route index element={<Home />} />
 			<Route path="/signup" element={<SignUp />} />
+			<Route path="/login" element={<Login />} />
 			<Route path="/confirm/:slugId?" element={<EmailConfirmation />} />
 		</Routes>
 	)
